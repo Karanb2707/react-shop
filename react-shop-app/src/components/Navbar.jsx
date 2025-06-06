1a28d98b5653c93f718129f8c3c93cb8bd92bb14
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-06-07 01:31:35 +0530

    Added navbar added styling

--- react-shop-app/src/components/Navbar.jsx
+++ react-shop-app/src/components/Navbar.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { FaCartShopping } from "react-icons/fa6";
+import Logo from "../assets/shoplogo.png"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <div className="flex flex-row items-center justify-evenly p-2 bg-red-700 drop-shadow-md drop-shadow-red-950">
+        <img src={Logo} className="h-10 rounded-md cursor-pointer transform hover:scale-105 duration-100" />
+
+        <div className="flex flex-row items-center justify-evenly gap-6">
+          <p className="text-white text-[16px] font-semibold cursor-pointer transform hover:scale-115 duration-100">Home</p>
+          <FaCartShopping className="text-white text-[16px] cursor-pointer transform hover:scale-115 duration-100"/>
+        </div>
+      </div>
+    </div>
   )
 }
 
