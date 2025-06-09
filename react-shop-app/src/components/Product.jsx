9e0b5821329ded4b5c2fec6a9813d8dd56b30d60
Karan Bolake <95834281+Karanb2707@users.noreply.github.com>
2025-06-09 10:18:06 +0000

    react 4

--- react-shop-app/src/components/Product.jsx
+++ react-shop-app/src/components/Product.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Product = ({post}) => {
+  const selected = false;
   return (
     <div className='flex flex-col items-center justify-between gap-2 shadow-md shadow-slate-800 p-3 m-2 rounded-2xl
       transform hover:scale-105 duration-300'>
@@ -14,7 +15,9 @@ const Product = ({post}) => {
       <div className='w-full flex flex-row items-center justify-between p-1 mt-2'>
         <p className='font-semibold text-green-800'>Price: {post.price} ₹</p>
         <button className='py-1 px-3 rounded-full ring ring-amber-800 bg-amber-600 text-white font-semibold'>
-          Add to cart
+          {
+            selected ? <p>Remove Item</p> : <p>Add to Cart</p>
+          }
         </button>
       </div>
     </div>
