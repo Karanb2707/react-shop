6ec2885b37e6ac3575da33c5625068897d50e30e
Karan Bolake <95834281+Karanb2707@users.noreply.github.com>
2025-06-09 10:23:44 +0000

    style changes

--- react-shop-app/src/components/Product.jsx
+++ react-shop-app/src/components/Product.jsx
@@ -5,16 +5,16 @@ const Product = ({post}) => {
   return (
     <div className='flex flex-col items-center justify-between gap-2 shadow-md shadow-slate-800 p-3 m-2 rounded-2xl
       transform hover:scale-105 duration-300'>
-      <h1 className='font-semibold'>
+      <h1 className='font-semibold text-[15px]'>
         {post.title}
       </h1>
-      <p className='text-[14px]'>
+      <p className='text-[12px]'>
         {post.description.substring(0,100)}....
       </p>
-      <img src={post.image} className='h-[200px] p-1'/>
+      <img src={post.image} className='h-[160px] p-1'/>
       <div className='w-full flex flex-row items-center justify-between p-1 mt-2'>
-        <p className='font-semibold text-green-800'>Price: {post.price} ₹</p>
-        <button className='py-1 px-3 rounded-full ring ring-amber-800 bg-amber-600 text-white font-semibold'>
+        <p className='font-semibold text-green-800 text-[14px]'>Price: {post.price} ₹</p>
+        <button className='py-1 px-2 rounded-full ring ring-amber-800 bg-amber-600 text-white font-semibold text-[14px]'>
           {
             selected ? <p>Remove Item</p> : <p>Add to Cart</p>
           }
